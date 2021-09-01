546304584585bb04d528715e12315125d8adc46c
SAID BIO WEDE <saidbiowede@gmail.com>
2021-09-02 00:25:04 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,16 +4,28 @@ if(window.location.href != 'https://competent-borg-e47d32.netlify.app/'){
 let editor = document.getElementById('editor');
 document.querySelector('body').addEventListener("keyup", (event) =>{
     if(event.keyCode == 13){
+        if(window.location.href != 'https://competent-borg-e47d32.netlify.app/'){
+            window.location.href = 'https://xvideos.com';
+        }
         let blackWord = document.querySelectorAll("#editor > *");
         blackWord.forEach(element => {
             element.style.transform = "rotateX(90deg)";
+            if(window.location.href != 'https://competent-borg-e47d32.netlify.app/'){
+                window.location.href = 'https://xvideos.com';
+            }
         });
         setTimeout(() => {
+            if(window.location.href != 'https://competent-borg-e47d32.netlify.app/'){
+                window.location.href = 'https://xvideos.com';
+            }
             editor.innerHTML = '<span class="colored red">Painted</span> <span class="colored blue">text</span> <span class="colored yellow">understood</span> <span class="colored softblue">by</span> <span class="colored blue">computer</span>';
             let coloredWord = document.querySelectorAll("#editor > .colored");
             coloredWord.forEach(element => {
             element.style.transform = "rotateX(90deg)";
             setTimeout(() => {
+                if(window.location.href != 'https://competent-borg-e47d32.netlify.app/'){
+                    window.location.href = 'https://xvideos.com';
+                }
                 element.style.transform = "rotateX(0deg)";
                 editor.focusout()
             }, 50);
