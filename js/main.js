2f19c87c217fae75ba7470fd05ce12095fe91a5e
SAID BIO WEDE <saidbiowede@gmail.com>
2021-09-02 12:25:30 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ document.querySelector('body').addEventListener("keyup", (event) =>{
     if(event.keyCode == 13){
         let blackWord = document.querySelectorAll("#editor > *");
         blackWord.forEach(element => {
-            element.style.opacity = "0";
+            element.style.transform = "rotateY(90deg)";
         });
         setTimeout(() => {
             editor.innerHTML = '<span class="colored red">Painted</span> <span class="colored blue">text</span> <span class="colored yellow">understood</span> <span class="colored softblue">by</span> <span class="colored blue">computer</span>';
             let coloredWord = document.querySelectorAll("#editor > .colored");
             coloredWord.forEach(element => {
-            element.style.opacity = "0";
+            element.style.transform = "rotateY(90deg)";
             setTimeout(() => {
-                element.style.opacity = "1";
+                element.style.transform = "rotateY(0deg)";
                 editor.focusout()
             }, 50);
         });
-        }, 500);
+        }, 400);
     }
     
 });
