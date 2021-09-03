711175ce260eb2e92ee8b3f9eea9bea387e107a7
SAID BIO WEDE <saidbiowede@gmail.com>
2021-09-03 15:48:54 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,22 +3,22 @@ document.querySelector('body').addEventListener("keyup", (event) =>{
     if(event.keyCode == 13){
         let blackWord = document.querySelectorAll("#editor > span");
         blackWord.forEach(element => {
-            element.style.width = element.offsetWidth+"px";
+            element.style.height = element.offsetHeight+"px";
         });
         let insideWord = document.querySelectorAll("#editor > span > span");
         insideWord.forEach(word => {
-            word.style.padding = "0px "+word.offsetWidth+"px 0px 0px";
+            word.style.padding = word.offsetHeight+"px 0px 0px 0px";
         });
         setTimeout(() => {
             editor.innerHTML = '<span class="colored red"><span>Painted</span></span> <span class="colored blue"><span>text</span></span> <span class="colored yellow"><span>understood</span></span> <span class="colored softblue"><span>by</span></span> <span class="colored blue"><span>computer</span></span>';
             let coloredWord = document.querySelectorAll("#editor > .colored");
             coloredWord.forEach(element => {
-                element.style.width = element.offsetWidth+"px";
+                element.style.height = element.offsetHeight+"px";
             });
             let insideWord = document.querySelectorAll("#editor > span > span");
             insideWord.forEach(word => {
                 word.style.transition = "none";
-                word.style.padding = "0px "+word.offsetWidth+"px 0px 0px";
+                word.style.padding = word.offsetHeight+"px 0px 0px 0px";
             });
             setTimeout(() => {
                 insideWord.forEach(word => {
@@ -28,7 +28,7 @@ document.querySelector('body').addEventListener("keyup", (event) =>{
             }, 10);
             setTimeout(() => {
                 coloredWord.forEach(element => {
-                    element.style.width = "initial";
+                    element.style.height = "initial";
                 });
             }, 500);
         }, 500);
