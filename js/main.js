3ceaf5176341d0c2c576eee1ec9e47a0c960f29d
SAID BIO WEDE <saidbiowede@gmail.com>
2021-09-03 14:47:01 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
 let editor = document.getElementById('editor');
 document.querySelector('body').addEventListener("keyup", (event) =>{
     if(event.keyCode == 13){
-        let blackWord = document.querySelectorAll("#editor > *");
+        let blackWord = document.querySelectorAll("#editor > span");
         blackWord.forEach(element => {
-            element.style.opacity = "0";
+            element.style.width = element.offsetWidth+"px";
+        });
+        let insideWord = document.querySelectorAll("#editor > span > span");
+        insideWord.forEach(word => {
+            word.style.padding = "0px "+word.offsetWidth+"px 0px 0px";
         });
         setTimeout(() => {
-            editor.innerHTML = '<span class="colored red">Painted</span> <span class="colored blue">text</span> <span class="colored yellow">understood</span> <span class="colored softblue">by</span> <span class="colored blue">computer</span>';
+            editor.innerHTML = '<span class="colored red"><span>Painted</span></span> <span class="colored blue"><span>text</span></span> <span class="colored yellow"><span>understood</span></span> <span class="colored softblue"><span>by</span></span> <span class="colored blue"><span>computer</span></span>';
             let coloredWord = document.querySelectorAll("#editor > .colored");
             coloredWord.forEach(element => {
-            element.style.opacity = "0";
+                element.style.width = element.offsetWidth+"px";
+            });
+            let insideWord = document.querySelectorAll("#editor > span > span");
+            insideWord.forEach(word => {
+                word.style.transition = "none";
+                word.style.padding = "0px "+word.offsetWidth+"px 0px 0px";
+            });
             setTimeout(() => {
-                element.style.opacity = "1";
-                editor.focusout()
-            }, 50);
-        });
+                insideWord.forEach(word => {
+                    word.style.transition = "ease .5s";
+                    word.style.padding = "0px 0px 0px 0px";
+                });
+            }, 10);
+            setTimeout(() => {
+                coloredWord.forEach(element => {
+                    element.style.width = "initial";
+                });
+            }, 500);
         }, 500);
     }
     
